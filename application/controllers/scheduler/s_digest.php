0047230ffd02f44a2bfab3e391284ea60067e8da
Laxman Maharjan <email@mlaxman.com.np>
2015-12-22 13:53:14 +0545

    digest header

--- application/controllers/scheduler/s_digest.php
+++ application/controllers/scheduler/s_digest.php
@@ -166,7 +166,7 @@ class S_Digest_Controller extends Controller {
 
 			$incident_head = '<b>'.$incident_count.' ';
 			$incident_head .= ($incident_count > 1) ? Kohana::lang('ui_main.incidents') : Kohana::lang('ui_main.incident');
-			$incident_head .= ' dated '.date("M d, Y").'</b><br/><br/>';
+			$incident_head .= ' as of '.date("M d, Y").'</b><br/><br/>';
 			
 			$message = $incident_head. $incident_msg_list . "<br/><br/>" .$company_note. $message . $message_end;
 			if(!empty($alert_incident)) {
