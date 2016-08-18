d6f0701fecdb7fea991543740ba86eb7ea34755c
Vaibhaw (Bob) Poddar <bob@himalayantechies.com>
2016-08-18 14:07:49 +0545

    as per the current live server

--- plugins/facebooksocial/views/facebook-social.php
+++ plugins/facebooksocial/views/facebook-social.php
@@ -1,7 +1,9 @@
+<!meta property="fb:app_id" content="140091756132653"!>
 <div class="content">
 
-<div id="fb-root"></div><script src="//connect.facebook.net/en_US/all.js#appId=140091756132653&xfbml=1"></script><fb:comments href="<?php echo url::base().trim($_SERVER['REQUEST_URI'],'/'); ?>" num_posts="10" width="530"></fb:comments>
+<div id="fb-root"></div><script src="//connect.facebook.net/en_US/sdk.js#&version=v2.5&appId=140091756132653&xfbml=1"></script><fb:comments href="<?php echo url::base().trim($_SERVER['REQUEST_URI'],'/'); ?>" num_posts="10" width="530"></fb:comments>
 
 </div>
 
 
+<!old src="//connect.facebook.net/en_US/all.js#appId=140091756132653&xfbml=1!">
