0728217e934d3f01a67f1b47a01f4e9165c02473
Laxman Maharjan <email@mlaxman.com.np>
2016-08-25 15:54:41 +0545

    location filter pcode

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -144,9 +144,8 @@ class Reports_Controller extends Main_Controller {
 				$this->template->content->location_filter += location_filter::get_location_filter_view($adm_level);
 			}
 		} else {
-			
 			foreach(location_filter::$admLevels as $adm_level => $admlvl) {
-				if($admlvl->dummy) {
+				if(!$admlvl->dummy) {
 					$this->template->content->location_filter += location_filter::get_location_filter_view($adm_level);
 				}
 			}
