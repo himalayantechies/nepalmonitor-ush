5bac57c4e7d459712877297ee57a8d214a99fa3d
Suman Shrestha <me.sthasuman@gmail.com>
2017-03-06 17:48:24 +0545

    defaulting

--- themes/default/views/reports/submit_custom_forms.php
+++ themes/default/views/reports/submit_custom_forms.php
@@ -383,7 +383,7 @@
 									});
 									if(".$field_value.") {
 										if($(\"#custom_field_".$field_id." option[value='".$field_value."']\").length <= 0) {
-											$(\"#custom_field_".$field_id."\").prepend('<option value=\"".$field_value."\">".$field_value."</option>'');
+											$(\"#custom_field_".$field_id."\").prepend('<option value=\"".$field_value."\">".$field_value."</option>');
 										}
 									}
 									$(\"#custom_field_".$field_id."\").val(\"".$field_value."\").trigger(\"change\");
